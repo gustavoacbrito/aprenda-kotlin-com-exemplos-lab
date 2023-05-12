b41faf6dcfebf658421a53c3c017dae8f01bd038
Gustavo Afonso Chaves de brito <121517266+gustavoacbrito@users.noreply.github.com>
2023-05-12 15:24:03 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,25 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+enum class Level { BASICO, INTERMEDIARIO, DIFICIL }
+data class User(val id: Int , val name: String)
+data class EducationalContent(val name: String, val duration: Int , val level: Level)
+data class Course(val name: String, var content: List<EducationalContent>) {
+    val subscribed = mutableListOf<User>()
+    fun enroll(user: User) {
+        subscribed.add(user)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val contents: List<EducationalContent> = listOf(EducationalContent("Conhecendo o Kotlin e Sua Documentação Oficial",120, Level.BASICO),
+        EducationalContent("Tratamento de exceções em Kotlin",60, Level.INTERMEDIARIO),
+        EducationalContent("Adicionando Segurança a uma API REST com Spring Security",240, Level.DIFICIL))
+    val user = User(1, "Rubens Ramos")
+    val kotlinCourse = Course("Kotlin", contents)
+    kotlinCourse.enroll(user)
+
+    println(kotlinCourse)
+    for (item in kotlinCourse.subscribed){
+        println(item)
+    }
 }
